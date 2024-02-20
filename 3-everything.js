f3c843df2abbee9bb967ed9b4142bcef18e7459e
sol11-code <143833266+sol11-code@users.noreply.github.com>
2024-02-19 19:38:29 -0800

    Update 3-everything.js

--- 3-everything.js
+++ 3-everything.js
@@ -1,5 +1,12 @@
 function every(array, test) {
-    // Your code here.
+  for (let element of array) {
+    if (!test(element)) return false;
+  }
+  return true;
+  }
+
+  function every(array, test) {
+    return !array.some(element => !test(element));
   }
   
   console.log(every([1, 3, 5], n => n < 10));
