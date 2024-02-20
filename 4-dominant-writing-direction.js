de15a2dea91febe1462b92ce5d0ccc03cabd5b02
sol11-code <143833266+sol11-code@users.noreply.github.com>
2024-02-19 19:49:18 -0800

    Update 4-dominant-writing-direction.js

--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,7 +1,13 @@
 require('./includes/scripts.js') // include the scripts used by the chapter
-
+  
 function dominantDirection(text) {
-    // Your code here.
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction: "none";
+  }).filter(({name}) => name != "none");
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a,b) => a.count > b.count ? a : b).require();
   }
   
   console.log(dominantDirection("Hello!"));
